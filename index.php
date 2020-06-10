81f2cb71064c7df4da2a53a97431a0000e7a965c
naamikaze <51897136+naamikaze@users.noreply.github.com>
2020-06-10 17:57:56 -0300

    Agregados

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/estilos.css">
+<?php
+    include("librerias.html");
+?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Melon</title>
+</head>
+<body>
+    <div class="container">
+        <br>
+        <header>
+            <li><h2>Melon</h2></li>
+        </header>
+        <h1>Titulo 1</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos labore ad reiciendis quibusdam ab, sint deserunt quasi facere excepturi. Labore, at corrupti illo porro enim unde nam maiores inventore laboriosam?</p>
+    </div>
+</body>
+</html>
