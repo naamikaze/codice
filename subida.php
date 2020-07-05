17394be7ea7591353d084a5a32c590a58962d94e
Mr-Pills <65681185+Mr-Pills@users.noreply.github.com>
2020-07-05 19:19:49 -0300

    Create subida.php

---
+++ subida.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/estilos.css">
+    <?php
+        include("librerias.html");
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subida de archivos</title>
+</head>
+<body>
+
+
+    <div class="container">
+
+        <label>Seleccione el archivo a subir *insertar insercion de archivos xD*</label><br><br>
+
+        <label>Descripción del archivo:</label><br><br>
+        <textarea rows="5" cols="100" placeholder="Redacte una descripción sobre el archivo" id="desc"></textarea>
+
+    </div>
+
+
+    
+</body>
+</html>
