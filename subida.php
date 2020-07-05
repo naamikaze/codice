9c153318f083c3e0a9489cd5b0cdc7387a4fb7fb
Mr-Pills <65681185+Mr-Pills@users.noreply.github.com>
2020-07-05 19:33:55 -0300

    Update subida.php

--- subida.php
+++ subida.php
@@ -17,7 +17,24 @@
         <label>Seleccione el archivo a subir *insertar insercion de archivos xD*</label><br><br>
 
         <label>Descripción del archivo:</label><br><br>
-        <textarea rows="5" cols="100" placeholder="Redacte una descripción sobre el archivo" id="desc"></textarea>
+        <textarea rows="5" cols="100" placeholder="Redacte una descripción sobre el archivo" id="desc"></textarea><br><br>
+
+        <label>Seleccione alguna de las materias</label><br>
+        <select class="form-control" id="materia">
+            <option value="no" disabled="disabled" selected="true">Seleccione una materia</option>
+            <option value="1">bombace</option>
+
+        </select><br><br>
+
+        <label>Seleccione alguno de los cursos</label><br>
+        <select class="form-control" id="curso">
+            <option value="no" disabled="disabled" selected="true">Seleccione un curso</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+
+        </select>
+
 
     </div>
 
