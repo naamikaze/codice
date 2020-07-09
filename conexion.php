774976a63701687bd5ddb15eb282f792ece41c24
Nacho <oo>
2020-07-09 16:53:55 -0300

    Creación de la bd y retoques
    Creación de la base de datos en el archivo melon.sql y ciertos retoques en el codigo, ademas de la eliminación de ciertos archivos basura.

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
     date_default_timezone_set('America/Argentina/Buenos_Aires');
     //Establezco la conexion con la base de datos.
-    $conexion = mysqli_connect("localhost", "root", "", "tpn1");
+    $conexion = mysqli_connect("localhost", "root", "", "melon");
 ?>
