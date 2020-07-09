9d5f2c48c236de0a642546bd1a79ea28ecb029b4
naamikaze <51897136+naamikaze@users.noreply.github.com>
2020-07-09 16:38:16 -0300

    Base de Datos
    Incluida la conexion hacia la bd, el backend y el JavaScript, falta agregar la subida de archivos.

---
+++ conexion.php
@@ -0,0 +1,5 @@
+<?php
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
+    //Establezco la conexion con la base de datos.
+    $conexion = mysqli_connect("localhost", "root", "", "tpn1");
+?>
