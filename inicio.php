6cea4285fd82a19672e55775b39189adc9b3c8b6
naamikaze <51897136+naamikaze@users.noreply.github.com>
2020-07-13 13:38:22 -0300

    Cambios menores

---
+++ inicio.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+
+    if ( isset( $_SESSION['user_id'] ) ) {
+       // ACA PUEDO HACER ALGO SI ESTA LOGUEADO !
+        include('librerias.html');
+         echo $_SESSION['user_id'];
+    } else {
+       echo $_SESSION['user_id'];
+        header("Location: index.php");
+    }   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+	     <meta charset="UTF-8">
+	     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	     <title>Document</title>
+     </head>
+     <body>
+         <h2>Bienvenido <?php echo $_SESSION['user_id']; ?></h2>
+         <p>¿Qué desea hacer?</p>
+         <a href="subida.php"><button>Subir Archivos</button></a>
+     </body>
+</html>
