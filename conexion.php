27a85ebea681e4347c62b8132e20893a8de5db67
Nacho <oo>
2020-09-23 18:55:06 -0300

    CAMBIAMOS TODO
    SE CAMBIA TODO MUCHACHOOOS

--- conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-    date_default_timezone_set('America/Argentina/Buenos_Aires');
-    //Establezco la conexion con la base de datos.
-    $conexion = mysqli_connect("localhost", "root", "", "melon");
-?>
