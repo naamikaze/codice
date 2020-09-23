308b51ed2e78e00439f13b9b74cd3c37a4d0cf15
Mr-Pills <65681185+Mr-Pills@users.noreply.github.com>
2020-09-23 20:53:22 -0300

    Agregados
    did u suck dicks?
    
    Co-Authored-By: naamikaze <51897136+naamikaze@users.noreply.github.com>

--- registro.php
+++ registro.php
@@ -6,9 +6,11 @@
     <title>Registro</title>
 </head>
 <body>
-    
 
-    <input type="text" id="email" placeholder="Ingrese un nombre de usuario">
+    <input type="text" id="name" placeholder="Ingrese su nombre">
+    <input type="text" id="ape" placeholder="Ingrese su apellido">
+    <input type="date" id="fecha" placeholder="Ingrese la fecha de su nacimiento">
+    <input type="text" id="username" placeholder="Ingrese un nombre de usuario">
     <input type="password" id="pass" placeholder="Ingrese una constraseña">
     <input type="submit" id="cargar" placeholder="Enviar">
     
