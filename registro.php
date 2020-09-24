e4b7f355f92ca26f553b71e83434cf280fd1f3a7
Nacho <oo>
2020-09-23 21:27:08 -0300

    cosas

--- registro.php
+++ registro.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+    include('librerias.html');
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
 </head>
 <body>
 
-    <input type="text" id="name" placeholder="Ingrese su nombre">
-    <input type="text" id="ape" placeholder="Ingrese su apellido">
-    <input type="date" id="fecha" placeholder="Ingrese la fecha de su nacimiento">
-    <input type="text" id="username" placeholder="Ingrese un nombre de usuario">
-    <input type="password" id="pass" placeholder="Ingrese una constraseña">
-    <input type="submit" id="cargar" placeholder="Enviar">
-    
+    <!-- Inputs para el registro !-->
+    <input type="text" id="nombre" placeholder="Ingrese su nombre">
+    <input type="text" id="apellido" placeholder="Ingrese su apellido">
+    <input type="date" id="fdenaci" placeholder="Ingrese la fecha de su nacimiento">
+    <input type="text" id="usuario" placeholder="Ingrese un nombre de usuario">
+    <input type="password" id="contra" placeholder="Ingrese una constraseña">
+   <button id="cargar">Registrarse</button> 
 </body>
 </html>
+<script>
+    // Se ejecuta la función cuando se aprieta el boton de 'subir'
+    document.getElementById('cargar').onclick = function(){
+
+        //Los id de los input se convierten en variables de JavaScript        
+        var nombre = 'joaco bayerri';
+        var apellido = $('#apellido').val();
+        var fdenaci = $('#fdenaci').val();
+        var usuario = $('#usuario').val();
+        var contra = $('#contra').val();
+
+        console.log($nombre)
+
+        $.post('modulo_registro.php', {nombre:nombre , apellido:apellido , fdenaci:fdenaci , usuario:usuario , contra:contra});
+
+        Swal.fire(
+  'Todo Listo!',
+  'Bienvenido a Códice',
+  'Success'
+)
+    }
+</script>
