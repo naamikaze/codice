9fc5f95f6aa5aba760e76465cf13aed6c7afa2bf
Nacho <oo>
2020-11-03 18:54:12 -0300

    Cagada

--- subida_archivo.php
+++ subida_archivo.php
@@ -1,3 +1,16 @@
+<?php
+    session_start();
+
+    if ( isset( $_SESSION['user_id'] ) ) {
+       // ACA PUEDO HACER ALGO SI ESTA LOGUEADO !
+    } else {
+       
+        header("Location: login.php");
+    }   
+
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,20 +29,20 @@
         <form method="POST" action="upload.php" enctype="multipart/form-data">
     <div>
     <span>Elija un archivo</span>
-      <input type="file" name="uploadedFile" />
+    <br>
+    <input id="archivo" type="file" name="archivo" required>
     </div>
- 
-    <input type="submit" name="uploadBtn" value="Subir" />
-  </form><br>
+<br>
 
         <label>Ingrese el título del archivo</label><br>
-        <input type="text" id="titulo" placeholder="Ingresar título"><br><br>
+        <input type="text" id="titulo" name="titulo" placeholder="Ingresar título"><br><br>
+
 
         <label>Descripción del archivo</label><br>
-        <textarea rows="5" cols="100" placeholder="Redacte una descripción sobre el archivo" id="descripcion"></textarea><br><br>
+        <textarea rows="5" cols="100" name="desc" placeholder="Redacte una descripción sobre el archivo" id="descripcion"></textarea><br><br>
 
         <label>Seleccione la materia</label><br>
-        <select class="form-control" id="materia">
+        <select class="form-control" id="materia" name="materia">
             <option value="no" disabled="disabled" selected="true">Materias</option>
             <option value="1">bombace</option>
             <option value="2">pancracio</option>
@@ -37,7 +50,7 @@
         </select><br><br>
 
         <label>Seleccione alguno de los cursos</label><br>
-        <select class="form-control" id="curso">
+        <select class="form-control" id="curso" name="curso">
             <option value="no" disabled="disabled" selected="true">Seleccione un curso</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -45,6 +58,18 @@
 
         </select>
 
+	<button id="cargar">Ingresar Archivo</button>
+  </form>
     </div>  
 </body>
 </html>
+   <script>
+       document.getElementById('cargar').onclick = function(){
+	   
+	   var nombre = document.getElementById('nombre').value;
+	   var desc = document.getElementById('desc').value;
+	   var materia = document.getElementById('materia').value;
+	   var curso = document.getElementById('curso').value;
+
+       }
+   </script>
---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php
+include('conexion.php');
+
+$archivo = addslashes(file_get_contents($_FILES['archivo']['tmp_name']));
+$titulo = $_POST['titulo']; 
+$desc = $_POST['desc']; 
+$materia = $_POST['materia']; 
+$curso = $_POST['curso']; 
+
+
+$profesor= "SELECT pk_profesores FROM profesores WHERE nombre AND apellido = '$profesor';
+
+$query = "INSERT INTO `archivos`( `nombre`, `descripcion`, `archivo`, `fk_materia`, `fk_curso`, `fk_profesor`) VALUES ('".$titulo."', '".$desc."' , '".$archivo."' , '".$materia."' , '".$curso."' , '".$profesor."')";
+
+mysqli_query($conexion,$query);
+
+
+    
+?>
