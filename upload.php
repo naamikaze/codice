d5bcad84ce11a920879e212a541b05e7db8cd21c
Nacho <oo>
2020-11-03 19:01:14 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -8,7 +8,7 @@ $materia = $_POST['materia'];
 $curso = $_POST['curso']; 
 
 
-$profesor= "SELECT pk_profesores FROM profesores WHERE nombre AND apellido = '$profesor';
+$profesor= "SELECT pk_profesores FROM profesores WHERE nombre AND apellido = '$profesor'";
 
 $query = "INSERT INTO `archivos`( `nombre`, `descripcion`, `archivo`, `fk_materia`, `fk_curso`, `fk_profesor`) VALUES ('".$titulo."', '".$desc."' , '".$archivo."' , '".$materia."' , '".$curso."' , '".$profesor."')";
 
